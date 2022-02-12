ae586daee5ad0f432342806ec9991bbd84a49138
prathap-koththigoda <51660157+prathap-koththigoda@users.noreply.github.com>
2022-02-12 10:05:42 +0100

    3rd update - element.html und generic.html gelöscht

--- assets/js/util.js
+++ assets/js/util.js
@@ -1,9 +1,6 @@
 (function($) {
 
-	/**
-	 * Generate an indented list of links from a nav. Meant for use with panel().
-	 * @return {jQuery} jQuery object.
-	 */
+	
 	$.fn.navList = function() {
 
 		var	$this = $(this);
